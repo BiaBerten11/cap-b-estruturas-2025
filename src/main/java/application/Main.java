3716942798f7763faab433c5dfbf7752df906a2d
Beatriz Berten <271332@unifio.edu.br>
2025-09-24 01:23:47 +0000

    aula 23/09

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import application.lista.ListaSimples;
+import application.pilha.PilhaSimples;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +13,16 @@ public class Main {
         lista.remover(1);
 
         System.out.println(lista);
+
+        PilhaSimples pilha = new PilhaSimples(3);
+        pilha.empilhar("Primeiro elemento");
+        pilha.empilhar("Segundo elemento");
+
+        System.out.println(pilha.desempilhar());
+        pilha.empilhar("Terceiro elemento");
+        System.out.println(pilha.desempilhar());
+        System.out.println(pilha.desempilhar());
+        System.out.println(pilha.desempilhar());
     }
 }
 
